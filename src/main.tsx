de5dc1ee475ab4ca1ca63e9eb55b75ea4bd45eb0
Cyrvan Bouchard <cyrvan@mbp-de-cyrvan.home>
2024-10-12 19:22:35 +0200

    chore: add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,13 @@ import "./index.css"
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
+
+// Create a client
+const queryClient = new QueryClient()
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -23,7 +27,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
